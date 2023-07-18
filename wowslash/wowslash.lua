4cc47824c8a79f980b1c2823904ba0f44c06c737
photonz <139331067+photonzz@users.noreply.github.com>
2023-07-18 09:38:39 +0000

    fix

--- wowslash/wowslash.lua
+++ wowslash/wowslash.lua
@@ -1,2 +1,14 @@
-message("Hello, World!")
- 
+-- Define a function to print or set the maxfps value
+local function maxFPS(value)
+    if value then
+        SetCVar("maxfps", value)
+        print("MaxFPS set to:", value)
+    else
+        local maxfps = GetCVar("maxfps")
+        print("MaxFPS value is:", maxfps)
+    end
+end
+      
+-- Define a slash command to execute the maxFPS function
+SLASH_MAXFPS1 = "/maxfps"
+SlashCmdList["MAXFPS"] = maxFPS
