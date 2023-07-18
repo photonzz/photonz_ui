21a1f61c5001863158d640f27bb2376ff535731d
photonz <139331067+photonzz@users.noreply.github.com>
2023-07-18 13:00:25 +0000

    made help

--- photonz_ui/photonz_ui.lua
+++ photonz_ui/photonz_ui.lua
@@ -1,11 +1,11 @@
 -- Define a function to print or set the maxfps value
-local function maxFPS(value)
+local function maxfps(value)
     if value then
         SetCVar("maxfps", value)
-        print("MaxFPS set to:", value)
+        print("maxfps set to:", value)
     else
         local maxfps = GetCVar("maxfps")
-        print("MaxFPS value is:", maxfps)
+        print("maxfps value is:", maxfps)
     end
 end
 
@@ -22,7 +22,7 @@ end
 
 -- Define a function to execute all of the useful commands
 local function photonz_ui_enable()
-    maxFPS(300) -- Set maxfps to 300
+    maxfps(300) -- Set maxfps to 300
     ffxglow(0) -- Disable ffxGlow effect
 end
 
@@ -32,12 +32,23 @@ local function photonz_ui_show()
     ffxglow() 
 end
 
+function photonz_ui_help()
+    print("List of slash commands for photonz_ui:")
+    print("/maxfps - set maximum fps limit")
+    print("/ffxglow - toggle FFX glow effect")
+    print("/photonz_ui_enable - enable all settings")
+    print("/photonz_ui_show - show current settings")
+    print("/photonz_ui_help - show help")
+end
+
 -- Define slash commands 
 SLASH_MAXFPS1 = "/maxfps"
 SLASH_FFXOFF1 = "/ffxglow"
 SLASH_PHOTONZUIENA1 = "/photonz_ui_enable"
 SLASH_PHOTONZUISHOW1 = "/photonz_ui_show"
-SlashCmdList["MAXFPS"] = maxFPS
+SLASH_PHOTONZUIHELP1 = "/photonz_ui_help"
+SlashCmdList["MAXFPS"] = maxfps
 SlashCmdList["FFXGLOW"] = ffxglow
 SlashCmdList["PHOTONZUIENA"] = photonz_ui_enable
 SlashCmdList["PHOTONZUISHOW"] = photonz_ui_show
+SlashCmdList["PHOTONZUISHELP"] = photonz_ui_help
