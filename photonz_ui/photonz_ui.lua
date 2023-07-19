4ae16dd104f06ff875887c9e627912b88937f1db
photonz <139331067+photonzz@users.noreply.github.com>
2023-07-19 16:07:35 +0200

    Update photonz_ui.lua

--- photonz_ui/photonz_ui.lua
+++ photonz_ui/photonz_ui.lua
@@ -1,58 +1,43 @@
-local function ShowClassColorInFriendlyNameplate(value)
-    if string.trim(value) ~= "" then -- if value parameter is not nil, set the cvar value
-        SetCVar("ShowClassColorInFriendlyNameplate", value)
-        local ShowClassColorInFriendlyNameplate = GetCVar("ShowClassColorInFriendlyNameplate")
-        print("ShowClassColorInFriendlyNameplate set to:", ShowClassColorInFriendlyNameplate)
-    else -- if no value parameter is provided, get the current cvar value
-        local ShowClassColorInFriendlyNameplate = GetCVar("ShowClassColorInFriendlyNameplate")
-        print("ShowClassColorInFriendlyNameplate value is currently set to:", ShowClassColorInFriendlyNameplate)
-    end
-end
+--[[--------------------------------------------------------------------
+photonz_ui
+----------------------------------------------------------------------]]
 
-local function maxfps(value)
-    if string.trim(value) ~= "" then -- if value parameter is not nil, set the cvar value
-        SetCVar("maxfps", value)
-        local maxfps = GetCVar("maxfps")
-        print("maxfps set to:", maxfps)
-    else -- if no value parameter is provided, get the current cvar value
-        local maxfps = GetCVar("maxfps")
-        print("maxfps value is currently set to:", maxfps)
-    end
-end
+local cvars = {
 
-local function nameplateOccludedAlphaMult(value)
-    if string.trim(value) ~= "" then -- if value parameter is not nil, set the cvar value
-        SetCVar("nameplateOccludedAlphaMult", value)
-        local nameplateOccludedAlphaMult = GetCVar("nameplateOccludedAlphaMult")
-        print("nameplateOccludedAlphaMult set to:", nameplateOccludedAlphaMult)
-    else -- if no value parameter is provided, get the current cvar value
-        local nameplateOccludedAlphaMult = GetCVar("nameplateOccludedAlphaMult")
-        print("nameplateOccludedAlphaMult value is currently set to:", nameplateOccludedAlphaMult)
-    end
-end
+    lootUnderMouse          = "1",
+    movieSubtitle                   = "1",
+    showTimestamps              = "%H:%M",
+    colorChatNamesByClass       = "1", -- no UI
 
-local function puihelp()
-    print("/puihelp")
-    print("/puienable")
-    print("/ShowClassColorInFriendlyNameplate 0 1")
-    print("/maxfps 0 300")
-    print("nameplateOccludedAlphaMult 0 1")
-end
+    ShowClassColorInFriendlyNameplate = "0",
+    nameplateOccludedAlphaMult = "1",
+    cameraDistanceMaxZoomFactor = "2.6", 
+    scriptErrors    = "1",
+    showTutorials   = "0",
 
-local function puienable()
-    maxfps(300) 
-    ShowClassColorInFriendlyNameplate (0)
-    nameplateOccludedAlphaMult(1)
-end
+    ffxGlow             = "0", 
+    ffxDeath             = "0", 
+    ffxNether             = "0", 
+    maxFPS              = "300",
+    violenceLevel         = "0", -- min 0, max 5
+    }
+    
+    local f = CreateFrame("Frame")
+    f:RegisterEvent("PLAYER_LOGIN")
+    f:SetScript("OnEvent", function()
 
-SlashCmdList.ShowClassColorInFriendlyNameplate = ShowClassColorInFriendlyNameplate
-SlashCmdList.maxfps = maxfps
-SlashCmdList.nameplateOccludedAlphaMult = nameplateOccludedAlphaMult
-SlashCmdList.puihelp = puihelp
-SlashCmdList.puienable = puienable
-
-SLASH_nameplateOccludedAlphaMult1 = "/nameplateOccludedAlphaMult"
-SLASH_ShowClassColorInFriendlyNameplate1 = "/ShowClassColorInFriendlyNameplate"
-SLASH_maxfps1 = "/maxfps"
-SLASH_puihelp1 ="/puihelp"
-SLASH_puienable1 ="/puienable"
+        -- Set friendly nameplate size
+        C_NamePlate.SetNamePlateFriendlySize(60, 30)
+        print("photonz_ui loaded")
+    
+        for cvar, value in pairs(cvars) do
+            local current = tostring(GetCVar(cvar))
+            if current ~= value then            
+                SetCVar(cvar, value)
+                -- Print message if CVar have been changed
+                C_Timer.After(1, function()
+                    print("|cffff9f7fSetCVar|r", cvar, "|cffff9f7f==>|r", value)
+                end)
+            end
+        end
+    end)
